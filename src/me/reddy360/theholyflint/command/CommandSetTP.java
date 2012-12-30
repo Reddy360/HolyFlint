651c13df11b21542c935d6b97ccb5c2bfdd08bac
Ffaen Walmsley <ffaenwalmsley@gmail.com>
2012-12-30 17:56:43 +0000

    Fixed command SetTP

--- src/me/reddy360/theholyflint/command/CommandSetTP.java
+++ src/me/reddy360/theholyflint/command/CommandSetTP.java
@@ -17,7 +17,6 @@ public class CommandSetTP implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label,
 			String[] args) {
-		Player player = (Player) sender;
 		if(!sender.hasPermission(pluginMain.pluginManager.getPermission("thf.settp"))){
 			sender.sendMessage(ChatColor.DARK_RED + "THERE ALL DEAD!");
 			return true;
@@ -25,8 +24,10 @@ public class CommandSetTP implements CommandExecutor {
 			sender.sendMessage(ChatColor.DARK_RED + "No player, no coords, no service.");
 			return true;
 		}else if(args.length == 1){
+			Player player = (Player) sender;
 			pluginMain.getConfig().set("coords." + args[0], player.getLocation().getWorld() + ":" + player.getLocation().getBlockX() 
 					+ ":" + player.getLocation().getBlockY() + ":" + player.getLocation().getBlockZ());
+			pluginMain.saveConfig();
 			player.sendMessage(ChatColor.GREEN + "TP Sign Coord set at" + pluginMain.getConfig().getString("coords." + args[0]));
 			return true;	
 		}
