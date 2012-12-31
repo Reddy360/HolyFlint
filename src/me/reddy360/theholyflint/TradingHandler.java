9bc26800fdee096eecae3c38473e36ad752c45b1
Ffaen Walmsley <ffaenwalmsley@gmail.com>
2012-12-31 20:09:39 +0000

    Added colors to trade handler, and changed Block.SIGNPOST.id to ItemSign.SIGN.id

--- src/me/reddy360/theholyflint/TradingHandler.java
+++ src/me/reddy360/theholyflint/TradingHandler.java
@@ -2,8 +2,10 @@ package me.reddy360.theholyflint;
 
 import net.minecraft.server.v1_4_6.Block;
 import net.minecraft.server.v1_4_6.Item;
+import net.minecraft.server.v1_4_6.ItemSign;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -15,35 +17,35 @@ public class TradingHandler {
 		ItemStack stick = new ItemStack(Item.STICK.id, 1);
 		ItemStack butter = new ItemStack(Block.GOLD_BLOCK.id);
 		ItemMeta metaButter = butter.getItemMeta();
-		metaButter.setDisplayName("You're trading on this side.");
+		metaButter.setDisplayName(ChatColor.LIGHT_PURPLE + "You're trading on this side.");
 		butter.setItemMeta(metaButter);
-		ItemStack sign = new ItemStack(Block.SIGN_POST.id, 1);
+		ItemStack sign = new ItemStack(ItemSign.SIGN.id , 1);
 		ItemMeta signMeta = sign.getItemMeta();
-		signMeta.setDisplayName("You're trading with " + otherPlayer.getName() + ".");
+		signMeta.setDisplayName(ChatColor.LIGHT_PURPLE + "You're trading with " + otherPlayer.getName() + ".");
 		sign.setItemMeta(signMeta);
 		ItemStack iron = new ItemStack(Block.IRON_BLOCK.id, 1);
 		ItemMeta ironMeta = iron.getItemMeta();
-		ironMeta.setDisplayName("The other persons trade items.");
+		ironMeta.setDisplayName(ChatColor.LIGHT_PURPLE + "The other persons trade items.");
 		iron.setItemMeta(ironMeta);
 		ItemStack trade = new ItemStack(Item.FLINT.id, 16);
 		ItemStack otherTrade = new ItemStack(Block.SANDSTONE.id, 64);
 		ItemStack canTrade = new ItemStack(Block.BEDROCK.id, 1);
 		ItemMeta canTradeMeta = canTrade.getItemMeta();
-		canTradeMeta.setDisplayName("Cannot make trade.");
+		canTradeMeta.setDisplayName(ChatColor.LIGHT_PURPLE + "Cannot make trade.");
 		canTrade.setItemMeta(canTradeMeta);
 		ItemStack confirm = new ItemStack(Block.WOOL.id, 1);
 		confirm.setDurability((short) 5);
 		ItemMeta confirmMeta = confirm.getItemMeta();
-		confirmMeta.setDisplayName("Accept trade?");
+		confirmMeta.setDisplayName(ChatColor.GREEN + "Accept trade?");
 		confirm.setItemMeta(confirmMeta);
 		ItemStack rejectTrade = new ItemStack(Block.WOOL.id, 1);
 		rejectTrade.setDurability((short) 14);
 		ItemMeta rejectTradeMeta = rejectTrade.getItemMeta();
-		rejectTradeMeta.setDisplayName("Reject trade?");
+		rejectTradeMeta.setDisplayName(ChatColor.RED + "Reject trade?");
 		rejectTrade.setItemMeta(rejectTradeMeta);
 		ItemStack partner = new ItemStack(Block.WOOL.id, 1);
 		ItemMeta partnerMeta = partner.getItemMeta();
-		partnerMeta.setDisplayName("Your partner has not decided.");
+		partnerMeta.setDisplayName(ChatColor.DARK_AQUA + "Your partner has not decided.");
 		partner.setItemMeta(partnerMeta);
 		ItemStack[] contents = {stick, air, butter, air, sign, air, iron, air, stick,
 				stick, stick, stick, stick, stick, stick, stick, stick, stick,
